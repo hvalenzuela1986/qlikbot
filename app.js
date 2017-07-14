ebce83145670deaca7957141eafd0dc6a8611802
hvalenzuela1986 <hvalenzuela1986@gmail.com>
2017-07-14 15:44:46 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,59 @@
-const moment = require('moment');
-const builder = require('botbuilder');
-const restify = require('restify');
-const server = restify.createServer();
-
-// Setup bot
-const connector = new builder.ChatConnector({
-    //appId: process.env.MICROSOFT_APP_ID,
-    //appPassword: process.env.MICROSOFT_APP_PASSWORD
-    appId: "ef4f80eb-823e-41a8-8974-1d47eaad7e4d",
-    appPassword: "ybggCUEWnoDVCUF7j0UXObt"
-});
-const bot = new builder.UniversalBot(connector);
-
-// Setup LUIS
-const recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/2b9b5b2a-0d70-47e0-a22b-349a2f92c4a0?subscription-key=25351cce1dd647f9b048f619c5b0a406&verbose=true&timezoneOffset=0&q=');
-const intents = new builder.IntentDialog({ recognizers: [recognizer] });
-
-// Setup Intents
-intents.matches('Saludar', function (session, results) {
-    session.send('Hola ¿En que te puedo ayudar?');
-});
-
-intents.matches('Produccion', [function (session, args, next) {
-    const division = ['FASA Norte', 'FASA Sur', 'FASA Centro'];
-    builder.Prompts.choice(session, 'Ahora mismo tenemos esa division disponible, ¿Cuál deseas revisar?', division);
-    //const divisionEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Division');
-    //if (divisionEntity) {
-      //  const match = builder.EntityRecognizer.findBestMatch(division, divisionEntity.entity);
-    //}
-    //if (!match) {
-      //  builder.Prompts.choice(session, 'Ahora mismo tenemos esa division disponible, ¿Cuál deseas revisar?', division);
-    //}
-    //else {
-   //     next({ response: match });
-    //}
-}, function (session, results) {
-    if (results.response) {
-        session.send("De acuerdo, la produccion de %s es 1", results.response.entity);
-    } else {
-        session.send('De acuerdo, pregunta nuevamente :)');
-    }
-}]);
-
-intents.onDefault(builder.DialogAction.send('No he entendido lo que quieres decir'));
-
-bot.dialog('/', intents);
+var builder = require('botbuilder');
+var restify = require('restify');
 
 // Setup Restify Server
-server.post('/api/messages', connector.listen());
+var server = restify.createServer();
 
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log('%s escuchando %s', server.name, server.url);
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create connector and listen for messages
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    //appId: "ef4f80eb-823e-41a8-8974-1d47eaad7e4d",
+    //appPassword: "ybggCUEWnoDVCUF7j0UXObt"
+});
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Disculpa, no he entendido tu pregunta');
+});
+
+// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/2b9b5b2a-0d70-47e0-a22b-349a2f92c4a0?subscription-key=25351cce1dd647f9b048f619c5b0a406&verbose=true&timezoneOffset=0&q=');
+bot.recognizer(recognizer);
+
+bot.dialog('Saludar', function (session) {
+    session.endDialog('Hola ¿En que te puedo ayudar?');
+}).triggerAction({
+    matches: 'Saludar'
+});
+
+bot.dialog('Produccion', [
+    function (session, args, next) {
+        //session.send('Estamos analizando tu consulta: \'%s\'', session.message.text);
+
+        // try extracting entities
+        var divisionEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Division');
+        if (divisionEntity) {
+            // city entity detected, continue to next step
+            session.dialogData.searchType = 'division';
+            next({ response: divisionEntity.entity });
+        } else {
+            // no entities detected, ask user for a destination
+            builder.Prompts.text(session, 'Por favor ingresa una division');
+        }
+    },
+    function (session, results) {
+        var result = results.response;
+
+        session.send('La producción de %s es 340.000 m3', result);
+       
+    }
+])
+.triggerAction({
+    matches: 'Produccion'
 });
